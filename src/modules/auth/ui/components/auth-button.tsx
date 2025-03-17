aa8a20616bac951f37f1288b7d2a1d08b06c9444
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-17 09:30:42 +0530

    side bar completed

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -2,12 +2,11 @@ import { Button } from "@/components/ui/button"
 import { UserCircleIcon } from "lucide-react"
 
 export const AuthButton = ()=>{
-    //TODO: ADD DIFFRENT AUTH STATES
+    //TODO: ADD difference AUTH STATES
     return (
         <Button
         variant="outline"
         className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none "
-        
         >
             <UserCircleIcon/>
             Sign in
