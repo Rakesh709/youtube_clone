3a85cfc9aa4bb1cfc68f41665d0b3103a675ff91
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-03-31 20:12:57 +0530

    sign-in and sign-out completed

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,15 +1,30 @@
-import { Button } from "@/components/ui/button"
-import { UserCircleIcon } from "lucide-react"
+"use-client";
 
-export const AuthButton = ()=>{
-    //TODO: ADD difference AUTH STATES
-    return (
-        <Button
-        variant="outline"
-        className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none "
-        >
-            <UserCircleIcon/>
+import { Button } from "@/components/ui/button";
+import { UserCircleIcon } from "lucide-react";
+
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+export const AuthButton = () => {
+  //TODO: ADD difference AUTH STATES
+  return (
+    <>
+        <SignedIn>
+            <UserButton/>
+            {/* Add menu items for studio and user profile */}
+        </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none "
+          >
+            <UserCircleIcon />
             Sign in
-        </Button>
-    )
-}
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
