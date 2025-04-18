0d34024644275fcb5ef589134deffd09e631ac80
Rakesh709 <kumar.rakesh1997@outlook.com>
2025-04-19 00:19:05 +0530

    sign in webhook setup

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,6 +1,9 @@
 import {Webhook} from 'svix';
 import {headers} from "next/headers";
 import {WebhookEvent} from "@clerk/nextjs/server"
+import { db } from '@/db';
+import { users } from '@/db/schema';
+
 
 
 export async function POST(req:Request){
@@ -17,4 +20,62 @@ export async function POST(req:Request){
     const headerPayload = await headers();
     const svix_id = headerPayload.get('svix-id');
     const svix_timestamp = headerPayload.get("svix-timestamp")
+    const svix_signature= headerPayload.get('svix-signature')
+
+    //IF THERE ARE NO HEADERS, ERROR OUT
+    if(!svix_id || !svix_timestamp || !svix_signature){
+        return new Response('Error:Missing Svix headers',{
+            status:400,
+        })
+    }
+
+    //GET BODY
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+
+    let evt : WebhookEvent
+
+    //VERIFY PAYLOAD WITH HEADERS
+
+    try {
+      evt = wh.verify(body,{
+        'svix-id':svix_id,
+        'svix-timestamp':svix_timestamp,
+        'svix-signature':svix_signature,
+      }) as WebhookEvent
+    } catch (err) {
+        console.error('Error: Could not verify webhook:',err);
+        return new Response('Error:Verification error',{
+            status:400,
+        })
+        
+    }
+
+    //DO SOMETHING WITH PAYLOAD
+    //FOR THIS GUID, LOG PAYLOAD TO CONSOLE
+    
+    const eventType = evt.type;
+
+    // console.log(`Recived webhook with id ${data.id} and event type of ${eventType}`);
+    // console.log('Webhook payload',body);
+
+    if(eventType ==='user.created'){
+        const {data} = evt;
+
+        await db.insert(users).values({
+            clerkId:data.id,
+            name:`${data.first_name} ${data.last_name}`,
+            //google sign in to get first +last name
+            imageUrl:data.image_url,
+
+        })
+    }
+
+    if(eventType==='user.deleted'){
+        const {data} = evt
+    }
+
+    return new Response('webhook recived',{status:200})
+    
+    
 }
